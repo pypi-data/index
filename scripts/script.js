2b691a12b4a752d1668231ed05fed05e458d9bcf
Tom Forbes <tom@tomforb.es>
2023-03-05 22:03:30 +0000

    Ok

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,16 @@ module.exports = async ({github, context}) => {
         sort: "full_name",
     });
 
-    return response.data.map(r => r.full_name)
+    let repo_names = response.data.map(r => r.full_name).filter(name => name.startsWith("pypi-data/pypi-code-")).map(name => name.split('/')[1]);
+
+    let indexes = [];
+    for (const name in repo_names) {
+        let content = await github.rest.repos.getContent({
+            owner: "pypi-data",
+            repo: name,
+            path: "index.json"
+        });
+        console.log(name);
+        console.log(content);
+    }
 }
